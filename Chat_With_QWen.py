f613385b1841b85b04b98b52f06910f99943c03f
GuoTong <acceptedbot@foxmail.com>
2024-03-27 16:44:09 +0800

    实现多轮对话功能

--- Chat_With_QWen.py
+++ Chat_With_QWen.py
@@ -7,22 +7,42 @@ import json
 def show_message_box(*args):
     pass
 
+# 处理图片，根据来源生成对应的api消息格式
+def dispose_input_to_message(role, user_input, images=[]):
+    content = []
+    content.append({'text': user_input})
+    for image in images:
+        content.append({"image": image})
+    messages = {
+        'role': role,
+        'content': content
+    }
+    return messages
+# 侦测网络图片是否存在
+def check_online_image_exist(image_url):
+    try:
+        r = requests.get(image_url)
+        return r.status_code
+        
+    except:
+        return 'Image does not exist!'
 
 # 本地图片上传接口
-def conversation_call_with_sdk(messages):
-    response = dashscope.MultiModalConversation.call(model='qwen-vl-plus',
+def conversation_call_with_sdk(messages,model='qwen-vl-plus',api_key='sk-ac227686a13348d9a5fcf3857c36de48'):
+    dashscope.api_key = api_key
+    response = dashscope.MultiModalConversation.call(model,
                                                      messages=messages)
     return response
 
 
 # 网络图片URL上传接口
-def conversation_call_with_http(messages):
+def conversation_call_with_http(messages,model='qwen-vl-plus',api_key='sk-ac227686a13348d9a5fcf3857c36de48'):
     model_url = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/multimodal-generation/generation'
 
     headers = {'Content-Type': 'application/json',
-               'Authorization': 'sk-ac227686a13348d9a5fcf3857c36de48'}
+               'Authorization': api_key}
     payload = {
-        'model': 'qwen-vl-plus',
+        'model': model,
         'input': {
             'messages': messages
         }
@@ -34,10 +54,14 @@ def conversation_call_with_http(messages):
 # 拆分响应包信息
 def dispose_response_message(response):
     status_code = response.status_code
-    response = json.loads(response.text)
+    try:
+        response = json.loads(response.text)
+    except Exception as e:
+        print(f'响应包错误，{e.__class__.__name__}:{e}')
     if status_code == HTTPStatus.OK:
         message = response['output']['choices'][0]['message']
     else:
+        #完成错误代码输出
         message = ''
         error_code = response['code']
         error_message = response['message']
--- homepage.py
+++ homepage.py
@@ -1,38 +1,16 @@
 import streamlit as st
 import Chat_With_QWen
-chat = None
+from http import HTTPStatus
+import requests
 
+ctyun_icon = open(r'figures\\CTyun_logo.png',mode='rb').read()
 st.set_page_config(
     page_title="ChatApp",
-    page_icon=" ",
-    layout="wide",
+    page_icon=ctyun_icon,
+    layout="wide"
 )
 st.title('欢迎使用通义千问大模型')
 
-
-# def increase_rows():
-#     if len(st.session_state['input_pictures']) < 5:
-#         st.session_state['input_pictures'].append('')
-
-# 处理图片，根据来源生成对应的api消息格式
-def dispose_input_to_message(role, user_input, images=[]):
-    content = []
-    content.append({'text': user_input})
-    for image in images:
-        content.append({"image": image})
-    messages = {
-        'role': role,
-        'content': content
-    }
-    return messages
-
-def check_online_image_exist(image_url):
-    import requests
-    try:
-        r = requests.get(image_url)
-        return r.status_code
-    except:
-        return 'Image does not exist!'
 
 def display_chat(message):
     role = message['role']
@@ -40,20 +18,23 @@ def display_chat(message):
     with st.chat_message(role):
         for info in content:
             for key,value in info.items():
-                print(type(info))
                 st.markdown(value, unsafe_allow_html=True)
 
 
+# 初始化对话信息
 if "history" not in st.session_state:
     st.session_state["history"] = []
-
 if 'image_upload_method' not in st.session_state:
     st.session_state["image_upload_method"] = "online"
-
+if "input_pictures" not in st.session_state:
+    st.session_state["input_pictures"] = []     
+    
+    
 for message in st.session_state["history"]:
-    st.markdown(message, unsafe_allow_html=True)
+    display_chat(message)
 
 with st.sidebar:
+    
     import tkinter as tk
     from tkinter import filedialog
 
@@ -63,11 +44,7 @@ with st.sidebar:
         index=1
     )
 
-    if "input_pictures" not in st.session_state:
-        st.session_state["input_pictures"] = []
-
     if option == "本地图片":
-        # with sidebar_placeholder.container():
         # 设置tkinter
         root = tk.Tk()
         root.withdraw()
@@ -85,81 +62,61 @@ with st.sidebar:
             )
             for picture_name in picture_names:
                 st.session_state['image_upload_method'] = 'local'
-                st.write(picture_name)
-                st.image(picture_name)
-                st.session_state["input_pictures"].append(picture_name)
-        st.write(st.session_state['input_pictures'])
+                # st.write(picture_name)
+                # st.image(picture_name)
+                st.session_state["input_pictures"].append(''.join(['file://',picture_name]))
+        # st.write(st.session_state['input_pictures'])
 
     elif option == "网络图片":
         picture_url = st.text_input('请输入图片的URL')
         clicked = st.button("添加图片URL")
-        ###
-        # st.write(len(st.session_state["input_pictures"]))
-
         if clicked and picture_url not in st.session_state["input_pictures"]:
-            exist_code = check_online_image_exist(picture_url)
-            if exist_code == '200':
+            exist_code = Chat_With_QWen.check_online_image_exist(picture_url)
+            if exist_code == HTTPStatus.OK:
                 st.session_state['input_pictures'].append(picture_url)
-                # st.image(picture_url)
             else:
                 st.text('填入的图片不存在')
-        ###
-        # st.write(st.session_state['input_pictures'])
+            
 
-    # if st.button("上传图片"):
-    #     st.session_state["input_pictures"] = []
-    # st.write(st.session_state['input_pictures'])
 
-# st.text(option)
 # user_input接收用户的输入
 if user_input := st.chat_input("Chat with 通义千问: "):
 
     # 格式化输入的信息
-    format_messages = dispose_input_to_message('user',
+    format_messages = Chat_With_QWen.dispose_input_to_message('user',
                                                user_input=user_input,
                                                images=st.session_state["input_pictures"])
+
+    st.session_state["input_pictures"] = []
     st.session_state['history'].append(format_messages)
+    
     # 在页面上显示用户的输入
     display_chat(format_messages)
 
     if st.session_state['image_upload_method'] == 'online':
         response = Chat_With_QWen.conversation_call_with_http(st.session_state['history'])
-
-    # st.write(format_messages)
-    # print(format_messages)
-    # st.write(response.text)
         status_code,response_message= Chat_With_QWen.dispose_response_message(response)
-        if status_code == '200':
+        if status_code == HTTPStatus.OK:
             st.session_state['history'].append(response_message)
+            # 展示大模型返回信息
+            display_chat(response_message)
         else:
             st.session_state['history'].pop()
-        display_chat(response_message)
-    # 重置sidebar中已输入的图片信息
-    # TODO 输入chat内容后清空图片输入信息
-    st.session_state["input_pictures"] = []
-    # st.write(st.session_state["input_pictures"])
-    # # 将用户的输入加入历史
-    # st.session_state.history.append(generate_session('user', user_input, image))
-    #
-    # # get_response_material用来获取模型生成的回复，这个是需要根据自己的情况去实现
-    # # response为大模型生成的回复，material为RAG的检索的内容
-    # status_code, response = get_response_material(st.session_state.history)
-    # response
-    # # TODO 问答需要交替，修改报错后的数组添加逻辑
-    # if status_code == 200:
-    #     response_content = response['output']['choices'][0]['message']['content']
-    #     # 在页面上显示模型生成的回复
-    #     with st.chat_message("assistant"):
-    #         st.markdown(response_content)
-    #
-    #     # 将模型的输出加入到历史信息中
-    #     # TODO 需要修改
-    #     st.session_state.history.append(generate_session('assistant', response_content))
-    #     st.session_state.history
-    #     # 只保留十轮对话，这个可根据自己的情况设定，我这里主要是会把history给大模型，context有限，轮数不能太多
-    #     if len(st.session_state.history) > 20:
-    #         st.session_state.messages = st.session_state.messages[-20:]
-    #
-    # else:
-    #     st.session_state.history.pop()
-    #     st.session_state.history
+            
+    else:
+        response = Chat_With_QWen.conversation_call_with_sdk(st.session_state['history'])
+        status_code,response_message= Chat_With_QWen.dispose_response_message(response)
+        # 成功返回信息则展示，错误则弹出会话
+        if status_code == HTTPStatus.OK:
+            st.session_state['history'].append(response_message)
+            # 展示大模型返回信息
+            display_chat(response_message)
+        else:
+            with st.chat_message('assistant'):
+                st.error('服务器响应异常')
+            st.session_state['history'].pop()
+            
+    #设置保存的最大历史会话数  
+    if len(st.session_state.history) > 20:
+        st.session_state.messages = st.session_state.messages[-20:]
+
